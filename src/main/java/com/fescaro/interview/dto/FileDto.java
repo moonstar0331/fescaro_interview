8f562b7cb2d051b9ed94b47a369b5fb93988a9bd
MoonStar <moonsung0331@gmail.com>
2024-03-12 12:28:30 +0900

    feat: 도메인 및 DTO 구현
    요구된 시스템을 구현하기 위한 엔티티와 DTO를 작성하였다.
    암호화 대상 파일과 암호화된 파일을 별도의 엔티티로 다루는 것을 고려하였지만,
    다운로드 기능 이외에 조회 등에서 계속 조인을 통해 이루어져야 하기 때문에 하나의 테이블에서 관리하도록 하였다.
    그리고 업로드 일시의 경우는 한 번에 적용되어야 하기 때문에 JPAAuditing을 사용하였다.

---
+++ src/main/java/com/fescaro/interview/dto/FileDto.java
@@ -0,0 +1,34 @@
+package com.fescaro.interview.dto;
+
+import com.fescaro.interview.entity.FileEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+public class FileDto {
+    private Long id;
+    private String originFileName;
+    private String originFilePath;
+    private String encryptedFileName;
+    private String encryptedFilePath;
+    private String iv;
+    private LocalDateTime createdAt;
+
+    public static FileDto from(FileEntity entity) {
+        return FileDto.builder()
+                .id(entity.getId())
+                .originFileName(entity.getOriginFileName())
+                .originFilePath(entity.getOriginFilePath())
+                .encryptedFileName(entity.getEncryptedFileName())
+                .encryptedFilePath(entity.getEncryptedFilePath())
+                .iv(entity.getIv())
+//                .ivBytes(entity.getIvBytes())
+                .createdAt(entity.getCreatedAt())
+                .build();
+    }
+}
