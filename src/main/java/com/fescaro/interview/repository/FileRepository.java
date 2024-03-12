b52092e649fbd6ad9de908ccadcf4cc21fe7585c
MoonStar <moonsung0331@gmail.com>
2024-03-12 21:20:17 +0900

    feat: 파일명에 따른 파일 엔티티 조회 메소드 작성
    테이블의 키 값을 프론트엔드 측에서 알 수 없게 되어 파일명을 통해서 다운로드 기능을 요청하기 때문에 파일명에 따른 파일 엔티티 조회가 필요하여 구현하였음

--- src/main/java/com/fescaro/interview/repository/FileRepository.java
+++ src/main/java/com/fescaro/interview/repository/FileRepository.java
@@ -4,6 +4,12 @@ import com.fescaro.interview.entity.FileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FileRepository extends JpaRepository<FileEntity, Long> {
+
+    Optional<FileEntity> findByOriginFileName(String fileName);
+    Optional<FileEntity> findByEncryptedFileName(String fileName);
+
 }
